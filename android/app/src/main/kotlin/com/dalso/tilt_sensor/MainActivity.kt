a69892130aaffdd67b9bf637279704dc7965a5b5
Dalso <flskwhdhk@gmail.com>
2023-12-21 15:17:19 +0900

    수평 측정기 앱 만들어보기 시작

---
+++ android/app/src/main/kotlin/com/dalso/tilt_sensor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dalso.tilt_sensor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/tilt_sensor.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TiltSensorApp extends StatefulWidget {
+  const TiltSensorApp({super.key});
+
+  @override
+  State<TiltSensorApp> createState() => _TiltSensorAppState();
+}
+
+class _TiltSensorAppState extends State<TiltSensorApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
