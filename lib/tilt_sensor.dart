1f8ef190fbcd2e2fa213667db290c0332869c7db
Dalso <flskwhdhk@gmail.com>
2023-12-21 15:50:32 +0900

    sensor 라이브러리를 활용해 휴대폰 센서값을 얻어 휴대폰을 움직일때마다 중앙 구체가 움직이는 앱 완성

--- lib/tilt_sensor.dart
+++ lib/tilt_sensor.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 class TiltSensorApp extends StatelessWidget {
   const TiltSensorApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // 가로모드 고정
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+    ]);
+
     // 화면 최대 width , height를 통해 중간값 얻기
     final centerX = MediaQuery.of(context).size.width / 2 - 50;
     final centerY = MediaQuery.of(context).size.height / 2 - 50;
@@ -12,19 +19,33 @@ class TiltSensorApp extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned(
-            left: centerX,
-            top: centerY,
-            child: Container(
-              // 박스 모양 커스텀
-              decoration: BoxDecoration(
-                color: Colors.green,
-                shape: BoxShape.circle
-              ),
-              width: 100,
-              height: 100,
-            ),
-          ),
+          StreamBuilder<AccelerometerEvent>(
+              stream: accelerometerEventStream(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: Center(
+                      // 로딩창 표시
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                }
+
+                final event = snapshot.data!;
+                List<double> accelrometerValues = [event.x, event.y, event.z];
+
+                return Positioned(
+                  left: centerX + event.y * 20,
+                  top: centerY + event.x * 20,
+                  child: Container(
+                    // 박스 모양 커스텀
+                    decoration: const BoxDecoration(
+                        color: Colors.green, shape: BoxShape.circle),
+                    width: 100,
+                    height: 100,
+                  ),
+                );
+              }),
         ],
       ),
     );
