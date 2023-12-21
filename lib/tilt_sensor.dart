7a67356d0e185fea8fde3ffe1c086ccbae71b952
Dalso <flskwhdhk@gmail.com>
2023-12-21 15:26:00 +0900

    미디어 쿼리 박스데코레이션 활용하여 정중앙에 구체 배치

--- lib/tilt_sensor.dart
+++ lib/tilt_sensor.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 
-class TiltSensorApp extends StatefulWidget {
+class TiltSensorApp extends StatelessWidget {
   const TiltSensorApp({super.key});
 
   @override
-  State<TiltSensorApp> createState() => _TiltSensorAppState();
-}
-
-class _TiltSensorAppState extends State<TiltSensorApp> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    // 화면 최대 width , height를 통해 중간값 얻기
+    final centerX = MediaQuery.of(context).size.width / 2 - 50;
+    final centerY = MediaQuery.of(context).size.height / 2 - 50;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            left: centerX,
+            top: centerY,
+            child: Container(
+              // 박스 모양 커스텀
+              decoration: BoxDecoration(
+                color: Colors.green,
+                shape: BoxShape.circle
+              ),
+              width: 100,
+              height: 100,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
